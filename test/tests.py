e4102aff1370a4ec39c00271566a12da998d57d5
Damien Garaud <damien.garaud@inria.fr>
2009-06-11 16:44:34 +0200

    Updated the unit tests.

--- test/tests.py
+++ test/tests.py
@@ -16,22 +16,9 @@
 import os, sys
 import unittest
 sys.path.insert(0, '../')
-from ensemble_generation import *
+import network
+import program_manager
 sys.path.pop(0)
-
-
-####################
-# GLOBAL VARIABLES #
-####################
-
-# The parameter configuration file.
-config_parameter = "../example/parameter.cfg"
-
-# The program configuration file.
-config_program = "../example/program.cfg"
-
-# The polyphemus directory.
-polyphemus_dir = "../../.."
 
 
 #############
@@ -193,131 +180,6 @@ class NetworkTestCase(unittest.TestCase):
         #                             'login@domain.com', self.message)
 
 
-###########################
-# ENSEMBLE PARAMETER TEST #
-###########################
-
-class EnsembleParameterTest(unittest.TestCase):
-    
-    def setUp(self):
-        # The loop number for the random number generation.
-        self.Nloop = 1000
-
-    def testLoadEnsembleParameter(self):
-        # Declarations.
-        ep = EnsembleParameter()
-
-        # Raises an Exception for a wrong file.
-        self.assertRaises(Exception, ep.LoadConfiguration, 'nofile_sorry')
-        self.assertRaises(Exception, ep.ReadIdEnsembleFile, 'nofile_sorry',
-                          True)
-        # An empty ensemble.
-        self.assert_(len(ep.id_ensemble) == 0)
-
-        # Equal lengths.
-        self.assert_(ep.Nmodel == 0)
-        self.assert_(len(ep.value) == len(ep.name))
-        self.assert_(len(ep.value) == ep.Nparameter)
-        self.assert_(len(ep.name) == ep.Nparameter)
-        self.assert_(len(ep.parameter_size) == ep.Nparameter)
-        for i in range(ep.Nparameter):
-            self.assert_(len(ep.value[i]) == ep.parameter_size[i])
-            self.assert_(len(ep.occurrence_frequency[i]) == ep.parameter_size[i])
-        
-        # Loading.
-        ep.LoadConfiguration(config_parameter)
-        if len(ep.config.input_data[0]) != 0:
-            Ntmp = ep.Nparameter - len(ep.config.input_data)
-        else:
-            Ntmp = ep.Nparameter
-
-        # Equal lengths.
-        self.assert_(len(ep.value) == len(ep.name))
-        self.assert_(len(ep.value) == ep.Nparameter)
-        self.assert_(len(ep.name) == ep.Nparameter)
-        self.assert_(len(ep.parameter_size) == ep.Nparameter)
-        self.assert_(len(ep.occurrence_frequency) == Ntmp)
-        for i in range(Ntmp):
-            self.assert_(len(ep.value[i]) == ep.parameter_size[i])
-            self.assert_(len(ep.occurrence_frequency[i]) \
-                             == ep.parameter_size[i])
-
-    def testGenerateEnsemble(self):
-        import random
-
-        # Declarations.
-        ep = EnsembleParameter(config_parameter)
-        if len(ep.config.input_data[0]) != 0:
-            Ntmp = ep.Nparameter - len(ep.config.input_data)
-        else:
-            Ntmp = ep.Nparameter
-
-        # Equal lengths.
-        self.assert_(len(ep.value) == len(ep.name))
-        self.assert_(len(ep.value) == ep.Nparameter)
-        self.assert_(len(ep.name) == ep.Nparameter)
-        self.assert_(len(ep.parameter_size) == ep.Nparameter)
-        self.assert_(len(ep.occurrence_frequency) == Ntmp)
-        for i in range(Ntmp):
-            self.assert_(len(ep.value[i]) == ep.parameter_size[i])
-            self.assert_(len(ep.occurrence_frequency[i]) \
-                             == ep.parameter_size[i])
-
-        # Generates an ensemble.
-        ep.GenerateIdEnsemble()
-        self.assert_(len(ep.id_ensemble) == ep.Nmodel)
-        for i in range(ep.Nmodel):
-            self.assert_(len(ep.id_ensemble[i]) == ep.Nparameter)
-
-        # Checks the ensemble.
-        self.assert_(ep.CheckIdEnsemble())
-
-        # For the random number generation.
-        for i in range(self.Nloop):
-            index_parameter = random.randint(0, Ntmp - 1)
-            choice_list = range(ep.parameter_size[index_parameter])
-            index_choice = ep.ParameterChoice(index_parameter)
-            self.assert_(index_choice in choice_list)
-
-
-#########################
-# ENSEMBLE PROGRAM TEST #
-#########################
-
-class EnsembleProgramTest(unittest.TestCase):
-    
-    def setUp(self):
-        pass
-        
-    def testLoadEnsembleProgram(self):
-        # Declaration.
-        p = EnsembleProgram()
-
-        # Raises an Exception for a wrong file.
-        self.assertRaises(Exception, p.Init, 'nofile_sorry', 'nofile_sorry')
-        self.assertRaises(Exception, p.ReadIdEnsembleFile, 'nofile_sorry')
-
-        # Loading.
-        p.Init(config_parameter, config_program)
-        
-        # Raises an Exception because the ensemble is empty.
-        self.assertRaises(Exception, p.GetGeneralDict, 0)
-        p.parameter.GenerateIdEnsemble()
-
-        # Raises an Exception because the 'config_replacement' is not set.
-        self.assertRaises(Exception, p.GetGeneralDict, 0)
-        
-        # Sets the Polyphemus directory.
-        p.SetPolyphemusDirectory(polyphemus_dir)
-
-        # Declaration of the ConfigReplacement object.
-        config_replacement = modules.ConfigPolair3D()
-        p.SetConfigReplacement(config_replacement)
-
-        # Gets the general dictionary for the first model.
-        d = p.GetGeneralDict(0)
-
-
 ##############
 # PROCESSING #
 ##############
@@ -326,45 +188,19 @@ class EnsembleProgramTest(unittest.TestCase):
 if __name__ == '__main__':
     # Deletes the modules from 'ensemble_generation'.
     # You don't have to reload 'ipython' for each changement in a module.
-    del sys.modules['ensemble_generation']
-    del sys.modules['ensemble_generation.ensemble_parameter']
-    del sys.modules['ensemble_generation.ensemble_program']
-    del sys.modules['ensemble_generation.function']
-    del sys.modules['ensemble_generation.modules']
-    del sys.modules['ensemble_generation.network']
-    del sys.modules['ensemble_generation.polyphemus']
+    del sys.modules['network']
+    del sys.modules['program_manager']
     sys.path.insert(0, '../')
-    from ensemble_generation import *
+    import network, program_manager
     sys.path.pop(0)
 
-    # Checks the files and directories.
-    if not os.path.isfile(config_parameter):
-        raise Exception, "Unable to find the file \"" \
-            + config_parameter + "\". Please change it in the file \"" \
-            + os.sys.argv[0] + "\"."
-    if not os.path.isfile(config_program):
-        raise Exception, "Unable to find the file \"" \
-            + config_program + "\". Please change it in the file \"" \
-            + os.sys.argv[0] + "\"."
-    if not os.path.isdir(polyphemus_dir):
-        raise Exception, "Unable to find the directory \"" \
-            + polyphemus_dir + "\". Please change it in the file \"" \
-            + os.sys.argv[0] + "\"."
-    
     # Host tests.
     suite_host = unittest.TestLoader()\
         .loadTestsFromTestCase(HostTestCase)
     # Network tests.
     suite_net = unittest.TestLoader()\
         .loadTestsFromTestCase(NetworkTestCase)
-    # EnsembleParameter tests.
-    suite_parameter = unittest.TestLoader()\
-        .loadTestsFromTestCase(EnsembleParameterTest)
-    # EnsembleProgram tests.
-    suite_program = unittest.TestLoader()\
-        .loadTestsFromTestCase(EnsembleProgramTest)
-    # TestSuite.
-    suite = unittest.TestSuite([suite_host, suite_net, suite_parameter,
-                                suite_program])
+    suite = unittest.TestSuite([suite_host, suite_net])
+    # Runs the tests.
     unittest.TextTestRunner(verbosity = 2).run(suite)
 
