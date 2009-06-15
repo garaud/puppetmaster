fbf6b54abf607823ba18d9a8c452b9692221377f
Damien Garaud <damien.garaud@inria.fr>
2009-06-15 12:45:39 +0200

    Initial import of 2 examples with a configuration file.

---
+++ example/program.py
@@ -0,0 +1,100 @@
+# Copyright (C) 2009 INRIA
+# Authors: Damien Garaud
+# 
+# This file an example for the module 'program_manager.py'.
+# 
+# This script is free; you can redistribute it and/or modify it under the
+# terms of the GNU General Public License as published by the Free Software
+# Foundation; either version 2 of the License, or (at your option) any later
+# version.
+#
+# This is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+# details.
+
+import os, sys, pwd, socket, time, platform
+sys.path.insert(0, '../')
+import program_manager
+sys.path.pop(0)
+
+
+#######################
+# Configuration Class #
+#######################
+
+print("""
+#######################
+# Configuration Class #
+#######################""")
+
+current_date = time.asctime()
+username = pwd.getpwuid(os.getuid())[0]
+computer = socket.gethostname()
+tmp = platform.dist()
+distribution = tmp[0] + '-' + tmp[1] + '_' + tmp[2]
+# A dictionnary used in the replacement.
+replacement = {'%date%': current_date,
+               '%username%': username,
+               '%computer%': computer,
+               '%distribution%': distribution}
+# An instance 'program_manager.Configuration'.
+config = program_manager.Configuration('./example.cfg', mode = 'random',
+                                       path = os.path.abspath('./'))
+# Sets the replacement.
+config.SetReplacementMap(replacement)
+# Proceeds replacement.
+config.Proceed()
+print "The replacement is done."
+print("See the file: \'" + config.file_list[0] + "\' \nand compare it with \'" \
+          + config.raw_file_list[0] + "\'.")
+print "\nDo not forget to remove it."
+
+
+#################
+# Program Class #
+#################
+
+print("""\n
+#################
+# Program Class #
+#################""")
+
+# Declaration.
+command_name = '/bin/ls'
+program = program_manager.Program(command_name)
+
+# Name of the command.
+cmd = program.Command()
+print "Command: ", cmd
+
+# Tries the program.
+program.Try()
+
+# Launches the program.
+program.Run()
+print "Information about the program:\n\n", program.log
+
+
+########################
+# ProgramManager Class #
+########################
+
+print("""\n
+########################
+# ProgramManager Class #
+########################""")
+
+# Declaration.
+command_list = ['/bin/ls', '/bin/pwd', '/bin/ps']
+program_ensemble = program_manager.ProgramManager()
+for prg in command_list:
+    program_ensemble.AddProgram(prg)
+
+# Runs the ensemble of programs.
+print 'Runs.'
+program_ensemble.Run()
+
+# Runs the ensemble of programs on remote hosts (thanks to 'network.py').
+print "\nRuns on remote hosts."
+program_ensemble.RunNetwork(delay = 2.)
---
+++ example/remote.py
@@ -0,0 +1,78 @@
+# Copyright (C) 2009 INRIA
+# Authors: Damien Garaud
+# 
+# This file an example for the module 'network.py'.
+# 
+# This script is free; you can redistribute it and/or modify it under the
+# terms of the GNU General Public License as published by the Free Software
+# Foundation; either version 2 of the License, or (at your option) any later
+# version.
+#
+# This is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+# details.
+
+import sys, socket, random
+sys.path.insert(0, '../')
+import network
+sys.path.pop(0)
+
+
+##################
+# ACCESS METHODS #
+##################
+
+# A network.Network instance.
+net = network.Network()
+
+# Gets the name of hosts in a list.
+host_list = net.GetHostNames()
+
+# Gets the load averages (1 minute) for each host (a list of tuples).
+load_avr_list = net.GetLoadAverages()
+
+# The minimal load average in the list.
+min_load_avr = min([x[1] for x in load_avr_list])
+
+# The list of available hosts with their number of free processors.
+available_host_list = net.GetAvailableHosts()
+
+# Gets the name of an available host.
+available_host = net.GetAvailableHost()
+
+
+######################
+# PROCESSING METHODS #
+######################
+
+command_name = "echo 'Hello World!'"
+
+# A random host.
+index = random.randint(0, len(net.hosts) - 1)
+random_host = net.hosts[index].name
+while (random_host == socket.gethostname() and len(net.hosts) != 1):
+    index = random.randint(0, len(net.hosts) - 1)
+    random_host = net.hosts[index].name
+
+# Launches an interactive command on a local host (without 2nd argument).
+status = net.LaunchInt(command_name)
+# 'status' must be equal to 0.
+# Launches an interactive command on a remote host.
+status = net.LaunchInt(command_name, random_host)
+
+# Launches a foreground command.
+(status, output) = net.LaunchFG(command_name)
+# On a remote host.
+(status, output) = net.LaunchFG(command_name, random_host)
+
+# Launches a foreground command.
+processus = net.LaunchBG(command_name)
+# On a remote host.
+processus = net.LaunchBG(command_name, random_host)
+# processus.poll() = 0 or -1 if the processus is not finished.
+
+# If you want to send a mail.
+message = "This is just a test."
+# net.SendMail(subject = '[netowrk.py]: Test', fromaddr = login@domail.com,
+#              toadrr = login@domain.com, msg = message)
