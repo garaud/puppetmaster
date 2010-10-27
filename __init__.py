80697f00e2f970e83c93f0f78cc7d2fd6bc4221c
Damien Garaud <damien.garaud@inria.fr>
2010-10-27 16:20:53 +0200

    Imported a new '__init__.py' in the root project directory.

---
+++ __init__.py
@@ -0,0 +1,18 @@
+# Copyright (C) 2010 INRIA - EDF R&D
+# Authors: Damien Garaud
+#
+# This file is part of the PuppetMaster project. It provides facilities to
+# deal with computations over a Linux network.
+#
+# This script is free; you can redistribute it and/or modify it under the
+# terms of the GNU General Public License as published by the Free Software
+# Foundation; either version 2 of the License, or (at your option) any later
+# version.
+#
+# This is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+# details.
+"""PuppetMaster
+Copyright (C) 2010 INRIA - EDF R&D.
+"""
